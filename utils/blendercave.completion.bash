9105d9c4a746bffc3e05401adce7f5c629cfe67b
Damien Touraine <damien.touraine@limsi.fr>
2013-01-11 18:15:37 +0100

    Switch to version 2.4

---
+++ utils/blendercave.completion.bash
@@ -0,0 +1,26 @@
+_blenderCave() {
+    local args cur opts
+
+    COMPREPLY=()
+
+    argc=${COMP_CWORD}
+
+    cur="${COMP_WORDS[argc]}"
+
+    case $argc in
+	1)
+	    opts="start startalone stop stopalone"
+	    COMPREPLY=( $(compgen -W "$opts" -- $cur ) )
+	    ;;
+	2)
+	    prev="${COMP_WORDS[argc-1]}"
+	    if [ "$prev" == "start" -o "$prev" == "startalone" ]; then
+		COMPREPLY=( $(compgen -d -S / $cur) $(compgen -f -X "!*.blend" $cur ) )
+	    fi
+	    ;;
+    esac
+
+
+}
+
+complete -F _blenderCave BlenderCave.py
